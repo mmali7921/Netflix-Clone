9417215537333bc09ea279b2e105dd1166dcb3e3
Muhammed Ali <mmali2004@gmail.com>
2025-02-18 23:32:57 +0530

    intial commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+
+import './App.css';
+import React from 'react';
+import NavBar from './Components/NavBar/NavBar';
+import Banner from './Components/Banner/Banner';
+import RowPost from './Components/RowPost/RowPost';
+
+function App() {
+  return (
+    <div className="App">
+      
+      <NavBar />
+      <Banner />
+      <RowPost/>
+   
+    </div>
+  );
+}
+
+export default App;
